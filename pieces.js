84a581e6b2a7bfc87fd9dc733eb767f3cd74c38b
Antoniel Cleyton <antonieldbms@gmail.com>
2021-10-17 19:39:45 -0300

    Indentation style implementation

--- pieces.js
+++ pieces.js
@@ -1,6 +1,9 @@
 // Parts Registration System
 
 let weight = 99.5;
+let listParts = ["Brake Disc", "Shock", "Crankshaft", "Sail", "Battery", "Radiator", "Belt", "Carter", "Ignition", "Clutch"]
+let numberPieces = listParts.length;
+let namePiece = "Thread" /*Space is contact as a character*/
 
 // Check if the weight is greater than 100
 
@@ -12,22 +15,16 @@ if (weight >= 100) {
 
 // List of pieces
 
-let listParts = ["Brake Disc", "Shock", "Crankshaft", "Sail", "Battery", "Radiator", "Belt", "Carter", "Ignition", "Clutch"]
-
-let numberPieces = listParts.length;
-
-if (numberPieces < 10) {
-    console.log("The list still has room for more pieces!")
-} else {
-    console.log("There is no space available in the list, the maximum capacity has been reached!")
-} 
+    if (numberPieces < 10) {
+        console.log("The list still has room for more pieces!")
+    } else {
+        console.log("There is no space available in the list, the maximum capacity has been reached!")
+    } 
 
 // Character verification
 
-let namePiece = "Thread" /*Space is contact as a character*/
-
-if(namePiece.length > 3) {
-    console.log("Part name is valid, can follow with registration");
-} else {
-    console.log("Invalid part name, name must be more than 3 characters")
-}
+        if(namePiece.length > 3) {
+            console.log("Part name is valid, can follow with registration");
+        } else {
+            console.log("Invalid part name, name must be more than 3 characters")
+        }
